b0c1d98441ab59650505c069452da6f126ce0b5a
deeptika <deeptika24@gmail.com>
2020-05-07 20:15:30 +0530

    Updated users create command

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 #users_controller.rb
 
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: User.order(:id).map { |user| user.to_pleasant_string }.join("\n")
   end
