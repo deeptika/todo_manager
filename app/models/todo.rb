cb508e934ca4c3dd536bde1c2b5b6bb63e784f03
deeptika <deeptika24@gmail.com>
2020-05-07 21:49:46 +0530

    Added todos erb for /todos

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -10,4 +10,20 @@ class Todo < ActiveRecord::Base
     display_date = due_today? ? nil : due_date
     "#{id}. #{display_status} #{todo_text} #{display_date}"
   end
+
+  def self.overdue
+    all.where("due_date < ? and (not completed)", Date.today).order(:due_date)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date  > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
 end
