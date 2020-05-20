e6f210f545ec785c72419b656c80f22e7535c1c6
deeptika <deeptika24@gmail.com>
2020-05-20 23:22:14 +0530

    Added checks for creating a valid user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,14 @@ class UsersController < ApplicationController
     last_name = params[:last_name]
     email = params[:email]
     password = params[:password]
-    new_user = User.create!(first_name: first_name, last_name: last_name, email: email, password: password)
-    session[:current_user_id] = new_user.id
-    redirect_to "/"
+    new_user = User.new(first_name: first_name, last_name: last_name, email: email, password: password)
+    if new_user.save
+      session[:current_user_id] = new_user.id
+      redirect_to "/"
+    else
+      flash[:error] = new_user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    end
   end
 
   def login
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   has_many :todos
   has_secure_password
 
+  validates :first_name, presence: true
+  validates :email, presence: true
+
   def to_pleasant_string
     "#{id}. #{first_name} #{last_name} #{email}"
   end
