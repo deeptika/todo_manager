019ae39c76a04851074bdae151ae8e731405cd2e
deeptika <deeptika24@gmail.com>
2020-05-21 16:27:12 +0530

    Updated database configurations

--- db/schema.rb
+++ db/schema.rb
@@ -10,54 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_143559) do
+ActiveRecord::Schema.define(version: 2020_05_21_105210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "carts", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "menu_item_id"
-    t.integer "quantity"
-    t.bigint "menu_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "menu_items", force: :cascade do |t|
-    t.bigint "menu_id"
-    t.string "menu_item_name"
-    t.float "menu_item_price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "menus", force: :cascade do |t|
-    t.string "menu_name"
-    t.boolean "active"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "order_items", force: :cascade do |t|
-    t.bigint "order_id"
-    t.bigint "menu_item_id"
-    t.string "menu_item_name"
-    t.float "menu_item_price"
-    t.integer "quantity"
-    t.float "total"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.bigint "user_id"
-    t.datetime "date"
-    t.datetime "delivered_at"
-    t.string "status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "todos", force: :cascade do |t|
     t.text "todo_text"
@@ -71,8 +27,6 @@ ActiveRecord::Schema.define(version: 2020_05_17_143559) do
     t.string "last_name"
     t.string "email"
     t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
